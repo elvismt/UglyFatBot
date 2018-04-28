5237ab3897437409fe8cd69c89f5f25ee9bae4d1
elvismt <elvismtt@gmail.com>
2018-04-28 11:15:39 -0300

    Update license

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-
-/**
- * To run this code, you must do the following:
- *
- * 1. Deploy this code to a server running Node.js
- * 2. Run `npm install`
- * 3. Update the VERIFY_TOKEN
- * 4. Add your PAGE_ACCESS_TOKEN to your environment vars
- *
- */
+// -*- JavaScript -*-
+// Copyright 2018 Elvis M. Teixeira <elvismtt@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 
 'use strict';
 const PAGE_ACCESS_TOKEN = 'USE YOURS';
+const PORT = process.env.PORT || 3000;
 
-// Imports dependencies and set up http server
-const 
+
+const
   request = require('request'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json());
 
 
-// Sets server port and logs message on success
-app.listen(3000, () => console.log('webhook is listening'));
+app.listen(PORT, () => console.log(`webhook is listening in port ${PORT}`));
 
-// Accepts POST requests at /webhook endpoint
+
 app.post('/webhook', (req, res) => {  
-
-  // Parse the request body from the POST
   let body = req.body;
 
-  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
     body.entry.forEach(function(entry) {
 
-      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
-
-      // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender ID: ' + sender_psid);
 
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
@@ -52,40 +54,30 @@ app.post('/webhook', (req, res) => {
       }
       
     });
-    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
 
   } else {
-    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
 
 
-// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = "CREATE ONE";
-  
-  // Parse params from the webhook verification request
+
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
-  // Check if a token and mode were sent
+
   if (mode && token) {
-  
-    // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Respond with 200 OK and challenge token from the request
+
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);      
     }
   }
@@ -95,15 +87,11 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
   let response;
   
-  // Checks if the message contains text
   if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
     response = {
       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
   } else if (received_message.attachments) {
-    // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     response = {
       "attachment": {
@@ -132,28 +120,24 @@ function handleMessage(sender_psid, received_message) {
     }
   } 
   
-  // Send the response message
   callSendAPI(sender_psid, response);    
 }
 
 function handlePostback(sender_psid, received_postback) {
-  console.log('ok')
-   let response;
-  // Get the payload for the postback
+  let response;
   let payload = received_postback.payload;
 
-  // Set the response based on the postback payload
   if (payload === 'yes') {
     response = { "text": "Thanks!" }
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }
-  // Send the message to acknowledge the postback
+
   callSendAPI(sender_psid, response);
 }
 
 function callSendAPI(sender_psid, response) {
-  // Construct the message body
+
   let request_body = {
     "recipient": {
       "id": sender_psid
@@ -161,7 +145,6 @@ function callSendAPI(sender_psid, response) {
     "message": response
   }
 
-  // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
     "qs": { "access_token": PAGE_ACCESS_TOKEN },
